82495bd8c279ddf230b333e8347174c7f366a32d
RIGAL <dorian.rigal@epitech.eu>
2019-12-03 15:59:35 +0100

    Add voiceRecuperation

--- voiceRecuperation/index.js
+++ voiceRecuperation/index.js
@@ -1,3 +1,41 @@
-const speech = require('@google-cloud/speech')
+const record = require('node-record-lpcm16');
+const speech = require('@google-cloud/speech');
 
-var client = speech.SpeechClient()
+const client = new speech.SpeechClient();
+
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'fr-FR';
+
+const request = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: false
+};
+
+const recognizeStream = client
+  .streamingRecognize(request)
+  .on('error', console.error)
+  .on('data', data =>
+    process.stdout.write(
+      data.results[0] && data.results[0].alternatives[0]
+        ? `Transcription: ${data.results[0].alternatives[0].transcript}\n`
+        : `\n\nReached transcription time limit, press Ctrl+C\n`
+    )
+  );
+
+record
+  .record({
+    sampleRateHertz: sampleRateHertz,
+    threshold: 0,
+    verbose: false,
+    recordProgram: 'sox',
+    silence: '10.0',
+  }).stream()
+  .on('error', console.error)
+  .pipe(recognizeStream);
+
+console.log('Listening, press Ctrl+C to stop.');
