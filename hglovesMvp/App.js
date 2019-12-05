33ce8fac1905b5a52149008cbde2d6b5f5e296e9
hugocodinach <hugo.codinach@epitech.eu>
2019-12-05 17:19:22 +0100

    [INIT] font

--- hglovesMvp/App.js
+++ hglovesMvp/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import { Dimensions } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
 
 import HomeComponent from './components/home/HomeComponent';
 
 const ScreenDim = Dimensions.get("window");
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-        <HomeComponent />
-    </View>
-  );
+class App extends React.Component {
+
+    state = {
+        fontLoaded: false,
+    };
+
+    async componentDidMount() {
+        await Font.loadAsync({
+            'open-sans': require('./assets/fonts/OpenSans.ttf'),
+            'open-sans-bold': require('./assets/fonts/OpenSansBold.ttf'),
+        });
+        this.setState({ fontLoaded: true });
+    }
+
+    render() {
+        const { fontLoaded } = this.state;
+
+        if (!fontLoaded)
+            return (
+                <View></View>
+            );
+        return (
+            <View style={styles.container}>
+                <HomeComponent />
+            </View>
+        );
+    }
 }
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: ScreenDim.width,
-    height: ScreenDim.height,
-  },
+    container: {
+        flex: 1,
+        width: ScreenDim.width,
+        height: ScreenDim.height,
+    },
 });
+
+export default App;
--- hglovesMvp/components/home/HomeComponent.js
+++ hglovesMvp/components/home/HomeComponent.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Dimensions } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 const ScreenDim = Dimensions.get("window");
+const imageWidth = ScreenDim.width * 80 / 100;
+const imageHeight = Math.round(imageWidth * 363 / 293);
 
 class HomeComponent extends React.Component {
+
     render() {
         return (
             <View style={styles.container}>
-                <Text>home</Text>
+                <View style={styles.headerContainer}>
+                    <Text style={{ ...styles.headerText, fontFamily: 'open-sans-bold' }}>Ceci est un MVP</Text>
+                    <Text style={{ ...styles.headerText, fontFamily: 'open-sans' }}>Veuillez cliquer sur Communiquer pour démarrer la simulation</Text>
+                </View>
+                <View style={styles.logoContainer}>
+                    <Image
+                        style={{ width: imageWidth, height: imageHeight }}
+                        source={require('../../assets/images/home/logo.png')}
+                    />
+                </View>
+                <View style={styles.footerContainer}>
+
+                </View>
             </View>
         );
     }
@@ -16,11 +31,34 @@ class HomeComponent extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        display: 'flex',
         width: '100%',
         height: '100%',
+        backgroundColor: '#1c3956',
+    },
+    headerContainer: {
+        display: 'flex',
+        width: '80%',
+        height: '20%',
+        marginLeft: '10%',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    headerText: {
+        textAlign: 'center',
+        color: 'white'
+    },
+    logoContainer: {
+        display: 'flex',
+        width: '80%',
+        marginLeft: '10%',
+        height: '100%',
+    },
+    footerContainer: {
+        display: 'flex',
+        width: '100%',
+        height: '10%',
     },
 });
 
