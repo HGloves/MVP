166e547599ea9be5508600c39b454ea129dd3ff2
RIGAL <dorian.rigal@epitech.eu>
2019-12-06 14:49:33 +0100

    feat(mainComponent) remove record button

--- hglovesMvp/components/main/MainComponent.js
+++ hglovesMvp/components/main/MainComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Dimensions } from 'react-native'
-import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Image, TextInput } from 'react-native';
+import RecordButton from './RecordButton'
 
 const ScreenDim = Dimensions.get("window");
 const imageWidth = ScreenDim.width * 80 / 100;
@@ -10,14 +11,7 @@ class MainComponent extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            onRecord: false,
-        }
     }
-
-    onPress = () => {
-        this.setState({onRecord: !this.state.onRecord});
-    };
 
     render() {
         return (
@@ -28,30 +22,9 @@ class MainComponent extends React.Component {
                 <View style={styles.lormContainer}>
                 </View>
                 <View style={styles.actionsContainer}>
-                    <TouchableOpacity style={styles.recordContainer}
-                                      onPress={this.onPress}>
-                        {!this.state.onRecord ?
-                            <>
-                                <View style={{ ...styles.buttonActionContainer, width: '70%'}}>
-                                    <Text style={{ ...styles.recordText, fontFamily: 'open-sans-bold' }}>Parler</Text>
-                                </View>
-                                <View style={{ ...styles.buttonActionContainer, width: '30%'}}>
-                                    <Image style={styles.buttonAction} source={require('../../assets/microphone.png')} />
-                                </View>
-                            </>
-                            :
-                            <>
-                                <View style={{ ...styles.buttonActionContainer, width: '70%'}}>
-                                    <Text style={{ ...styles.recordText, fontFamily: 'open-sans-bold' }}>En cours</Text>
-                                </View>
-                                <View style={{ ...styles.buttonActionContainer, width: '30%'}}>
-                                    <Image style={styles.buttonAction} source={require('../../assets/record.png')} />
-                                </View>
-                            </>
-                        }
-                    </TouchableOpacity>
+                    <RecordButton/>
                     <View style={styles.inputContainer}>
-                        <TextInput style={styles.input}placeholder={"Bonjour"}/>
+                        <TextInput style={styles.input} placeholder={"Bonjour"} />
                     </View>
                 </View>
             </View>
@@ -84,27 +57,6 @@ const styles = StyleSheet.create({
         left: 0,
         width: '100%',
         height: '8%',
-    },
-    recordContainer: {
-        flex: 1,
-        flexDirection: 'row',
-        backgroundColor: '#1C3956',
-        borderTopRightRadius: 30,
-        height: '100%',
-        width: '30%',
-    },
-    recordText: {
-        color: '#FFFFFF',
-        fontSize: 20,
-    },
-    buttonActionContainer: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    buttonAction: {
-        width: 32,
-        height: 32,
     },
     inputContainer: {
         height: '100%',
