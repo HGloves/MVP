eac4db76b30491ee4071a57c1715944e856f0747
Nicolas Albiges <nalbiges22@gmail.com>
2019-12-09 17:23:55 +0100

    Feat(component): fix the annimation, can make multiples animations

--- hglovesMvp/components/annimation/AnnimationComponent.js
+++ hglovesMvp/components/annimation/AnnimationComponent.js
@@ -1,6 +1,6 @@
 import React, {Component, useState } from 'react';
 import { Dimensions } from 'react-native'
-import { StyleSheet, Text, View, TextInput, Animated} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Animated, Image, Button} from 'react-native';
 import { TouchableWithoutFeedback } from "react-native-web";
 
 const ScreenDim = Dimensions.get("window");
@@ -8,21 +8,16 @@ const ScreenDim = Dimensions.get("window");
 export default class AnnimationComponent extends Component  {
     constructor(props) {
         super(props);
-        this.state = {text: ""};
-        this.lorm = [1,
-            {animFade: new Animated.Value(0), moveAnim: new Animated.ValueXY({x: 100, y: 100}), dx: 10, dy: 300, fade: 0, letter: "b"},
-            3,4,5,6,7,8,9,10,11,12,13,14,
-            {anim: new Animated.ValueXY({x:10, y:10}), dx: 10, dy: 10, fade: 0, letter: 'o'},
-            16,17,18,19,20,21,22,23,24,25,26];
+        this.state = {text : "bab"};
+        this.lormPos = new Map([
+            ["0", {animFade: new Animated.Value(0), moveAnim: new Animated.ValueXY({x: 50, y: 100})}],
+            ["a", {animFade: new Animated.Value(0), moveAnim: new Animated.ValueXY({x: 50, y: 100}), animType: this.staticElement, xEnd: 50, yEnd: 100}],
+            ["b", {animFade: new Animated.Value(0), moveAnim: new Animated.ValueXY({x: 150, y: 100}), animType: this.moveElement, xStart: 150, yStart: 100, xEnd: 150, yEnd: 250}]
+        ]);
     }
 
-
-    moveElement(letter) {
-        console.log("moving");
-        let obj = this.lorm.find(function (elem) {
-            if (elem.letter !== undefined && elem.letter === letter.toLowerCase())
-                return elem
-        });
+    moveElement = (obj, callback) => {
+        console.log("mooving", obj.moveAnim.y, obj.yEnd)
         if (obj !== undefined) {
             Animated.sequence([
                 Animated.timing(obj.animFade, {
@@ -30,44 +25,72 @@ export default class AnnimationComponent extends Component  {
                     duration: 1000
                 }),
                 Animated.timing(obj.moveAnim, {
-                    toValue: {x: 100, y: 200},
+                    toValue: {x: obj.xEnd, y: obj.yEnd},
                     duration: 100
                 }),
                 Animated.timing(obj.animFade, {
                     toValue: 0,
                     duration: 1000
+                }),
+                Animated.timing(obj.moveAnim, {
+                    toValue: {x: obj.xStart, y: obj.yStart},
+                    duration: 100
                 })
-            ]).start()
+            ]).start(callback)
         }
     }
 
-    /*   whichLetters() {
-           if (this.state.text.length > 0) {
-               let word = this.state.text.split("");
-               for (let i = 0; word.length > i;i++) {
-                   console.log("lette r: " + word[i].toLowerCase() + " = lorm: " + this.alphabet.lorm[this.alphabet.letters.indexOf(word[i].toLowerCase())]);
-               }
-           }
-       };, opacity: this.fadeAnim*/
+    staticElement = (obj, callback) => {
+        console.log("fade")
+        if (obj !== undefined) {
+            Animated.sequence([
+                Animated.timing(obj.moveAnim, {
+                    toValue: {x: obj.xEnd, y: obj.yEnd},
+                    duration: 100
+                }),
+                Animated.timing(obj.animFade, {
+                    toValue: 1,
+                    duration: 1000
+                }),
+                Animated.timing(obj.animFade, {
+                    toValue: 0,
+                    duration: 1000
+                })
+            ]).start(callback)
+        }
+    }
+
+    whichLetters = () => {
+        console.log("Before annimation : " + this.state.text[0])
+        obj = this.lormPos.get(this.state.text[0])
+        obj.animType(obj , () => {
+            this.setState({text : this.state.text.substr(1)}, () => {
+                if (this.state.text == "") {
+                    console.log("QUIT\n")
+                    return;
+                }
+                console.log (this.state.text + " After\n");
+                this.whichLetters();
+            });
+        });
+    }
 
     render() {
         return (
             <View style={styles.container}>
                 <Animated.View style={{display: 'flex',
-                            transform: [{translateX: this.lorm[1].moveAnim.x}, {translateY: this.lorm[1].moveAnim.y}],
+                            transform: [{translateX: this.lormPos.get(this.state.text[0] == undefined ? "0" : this.state.text[0]).moveAnim.x}, {translateY: this.lormPos.get(this.state.text[0] == undefined ? "0" : this.state.text[0]).moveAnim.y}],
                             justifyContent: 'center',
                             alignItems: 'center',
                             backgroundColor: 'greenyellow',
                             borderRadius: 200,
                             width: 100,
                             height: 100,
-                            opacity: this.lorm[1].animFade}}>
-                    <TouchableWithoutFeedback style={styles.button} onPress={() => this.moveElement("b")}>
-                        <View>
-                            <Text style={styles.buttonText}>Press</Text>
-                        </View>
-                    </TouchableWithoutFeedback>
+                            opacity: this.lormPos.get(this.state.text[0] == undefined ? "0" : this.state.text[0]).animFade}}>
                 </Animated.View>
+                <Button
+          title="Press me"
+          onPress={() => this.whichLetters()} />
             </View>
         );
     }
@@ -77,6 +100,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#ecf0f1',
+    },
+    handContainer: {
+        width: '100%',
+        height: '78%',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
     tennisBall: {
         display: 'flex',
