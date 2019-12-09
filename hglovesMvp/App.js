c7c5bbfa1810386ae9ae4aa30b887f2841af1614
Baptiste <baptiste.cayrier@epitech.eu>
2019-12-09 11:05:04 +0100

    [Feat][master]: Add lorm identification (but not R and F)

--- hglovesMvp/App.js
+++ hglovesMvp/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Dimensions } from 'react-native'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, PanResponder} from 'react-native';
 
 import HomeComponent from './components/home/HomeComponent';
+import MainComponent from './components/main/MainComponent';
 
 const ScreenDim = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <HomeComponent />
+        <MainComponent />
     </View>
   );
 }
